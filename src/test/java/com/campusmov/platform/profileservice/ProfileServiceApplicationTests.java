4334d099575eef6f9f0d40c300c0d52321684382
elmer-riva <elmeraugustoriva@gmail.com>
2025-05-05 10:59:08 -0500

    Initial commit

---
+++ src/test/java/com/campusmov/platform/profileservice/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusmov.platform.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
