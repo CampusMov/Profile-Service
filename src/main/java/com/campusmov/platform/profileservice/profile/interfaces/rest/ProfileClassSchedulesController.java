7945964997469993f3f3a10f0b6aaba543b04deb
elmer-riva <elmeraugustoriva@gmail.com>
2025-05-07 18:02:48 -0500

    feat(class-schedules): expose GET /profiles/{id}/class-schedules endpoint
    - Add ProfileClassSchedulesController to handle incoming requests
    - Leverage ProfileQueryService to fetch and map a profile’s class schedules

---
+++ src/main/java/com/campusmov/platform/profileservice/profile/interfaces/rest/ProfileClassSchedulesController.java
@@ -0,0 +1,39 @@
+package com.campusmov.platform.profileservice.profile.interfaces.rest;
+
+import com.campusmov.platform.profileservice.profile.domain.model.queries.GetProfileByIdQuery;
+import com.campusmov.platform.profileservice.profile.domain.services.ProfileCommandService;
+import com.campusmov.platform.profileservice.profile.domain.services.ProfileQueryService;
+import com.campusmov.platform.profileservice.profile.interfaces.rest.resources.ClassScheduleResource;
+import com.campusmov.platform.profileservice.profile.interfaces.rest.resources.CreateClassScheduleResource;
+import com.campusmov.platform.profileservice.profile.interfaces.rest.resources.UpdateClassScheduleResource;
+import com.campusmov.platform.profileservice.profile.interfaces.rest.transform.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/profiles/{id}/class-schedules")
+@Tag(name = "Profiles Class Schedules", description = "Available Profile Class Schedules Endpoints")
+public class ProfileClassSchedulesController {
+    private final ProfileQueryService profileQueryService;
+    private final ProfileCommandService profileCommandService;
+
+    public ProfileClassSchedulesController(ProfileQueryService profileQueryService, ProfileCommandService profileCommandService) {
+        this.profileQueryService = profileQueryService;
+        this.profileCommandService = profileCommandService;
+    }
+
+    @GetMapping
+    @Operation(summary = "Get class schedules by profile ID")
+    public ResponseEntity<List<ClassScheduleResource>> getClassSchedulesByProfileId(@PathVariable String id) {
+        var getProfileByIdQuery = new GetProfileByIdQuery(id);
+        var profile = profileQueryService.handle(getProfileByIdQuery);
+        if (profile.isEmpty()) return ResponseEntity.notFound().build();
+        var classSchedules = ClassSchedulesFromValueObjectListAssembler.toResources(profile.get().getAcademicInformation().getClassSchedules());
+        return ResponseEntity.ok(classSchedules);
+    }
+}
