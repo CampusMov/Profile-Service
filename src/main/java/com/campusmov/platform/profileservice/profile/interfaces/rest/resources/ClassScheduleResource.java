57914d73442e54288eb29e241201114bbd59fbcc
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-06-17 17:03:09 -0500

    refactor: change change startedAt and endedAt from LocalDateTime to LocalTime

--- src/main/java/com/campusmov/platform/profileservice/profile/interfaces/rest/resources/ClassScheduleResource.java
+++ src/main/java/com/campusmov/platform/profileservice/profile/interfaces/rest/resources/ClassScheduleResource.java
@@ -1,16 +1,16 @@
 package com.campusmov.platform.profileservice.profile.interfaces.rest.resources;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record ClassScheduleResource(
         String id,
         String courseName,
         String locationName,
         String address,
-        double latitude,
-        double longitude,
-        LocalDateTime startedAt,
-        LocalDateTime endedAt,
+        Double latitude,
+        Double longitude,
+        LocalTime startedAt,
+        LocalTime endedAt,
         String selectedDay
 ) {
 }
